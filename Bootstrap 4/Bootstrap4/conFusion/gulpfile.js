21ed7b50a14887dc03a6f2d1a57670cdf2f7164b
Hesham Watany <hesham.watany@gmail.com>
2019-02-22 18:51:24 +0200

    Added Tasks (sass:Watch, browser-sync)

--- Bootstrap 4/Bootstrap4/conFusion/gulpfile.js
+++ Bootstrap 4/Bootstrap4/conFusion/gulpfile.js
@@ -1,29 +1,34 @@
 'use strict';
+
 var gulp = require('gulp'),
     sass = require('gulp-sass'),
     browserSync= require('browser-sync');
 
-gulp.task('sass', function() {
-    return gulp.src('/.css/*.scss')
-    .pipe(sass().on('error',sass.logError))
-    .pipe(gulp.dest('./css'));
-});
+
+gulp.task('sass', function () {
+    return gulp.src('./css/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('./css'));
+    });
 
 
 gulp.task('sass:watch', function() {
-    gulp.watch('./css/*.scss', ['sass'])
+    gulp.watch('./css/*.scss', ['sass']);
 });
 
 gulp.task('browser-sync', function() {
     var files = [
         './*.html',
+        '.index.html',
         './css/*.css',
-        './js/*.js',
-        './*.{png,jpg,gif}'
+        './img/*.{png,jpg,gif}',
+        './js/*.js'
+        
     ];
+
     browserSync.init(files, {
         server: {
-            baseDir: './'
+            baseDir: "./"
         }
     });
 });
