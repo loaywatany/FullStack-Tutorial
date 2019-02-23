a94e9c7f1128c4b49d578d628f8af42b9735d868
Hesham Watany <hesham.watany@gmail.com>
2019-02-23 05:54:56 +0200

    Added comments to gulp file

--- Bootstrap 4/Bootstrap4/conFusion/gulpfile.js
+++ Bootstrap 4/Bootstrap4/conFusion/gulpfile.js
@@ -15,67 +15,66 @@ var gulp = require('gulp'),
 
 gulp.task('sass', function () {
     return gulp.src('./css/*.scss')
-        .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest('./css'));
-    });
+      .pipe(sass().on('error', sass.logError))
+      .pipe(gulp.dest('./css'));
+  });
 
-
-gulp.task('sass:watch', function() {
-    gulp.watch('./css/*.scss', ['sass']);
+gulp.task('sass:watch', function () {
+  gulp.watch('./css/*.scss', ['sass']);
 });
-
-gulp.task('browser-sync', function() {
+gulp.task('browser-sync', function () {
     var files = [
-        './*.html',
-        '.index.html',
-        './css/*.css',
-        './img/*.{png,jpg,gif}',
-        './js/*.js'
-        
+       './*.html',
+       './css/*.css',
+       './img/*.{png,jpg,gif}',
+       './js/*.js'
     ];
 
     browserSync.init(files, {
         server: {
-            baseDir: "./"
+           baseDir: "./"
         }
-    });
-});
+     });
+  
+  });
 
-
+// Default task
 gulp.task('default', ['browser-sync'], function() {
     gulp.start('sass:watch');
 });
 
+// Clean
 gulp.task('clean', function() {
     return del(['dist']);
 });
 
 gulp.task('copyfonts', function() {
-    gulp.src('./node_modules/font-awesome/fonts/**/*.{ttf,woff,eof,svg}*')
-    .pipe(gulp.dest('./dist/fonts'));
+   gulp.src('./node_modules/font-awesome/fonts/**/*.{ttf,woff,eof,svg}*')
+   .pipe(gulp.dest('./dist/fonts'));
 });
 
+// Images
 gulp.task('imagemin', function() {
     return gulp.src('img/*.{png,jpg,gif}')
-    .pipe(imagemin({ optimizationLevel: 3, progressive: true, interlaced: true }))
-    .pipe(gulp.dest('dist/img'));
-});
+      .pipe(imagemin({ optimizationLevel: 3, progressive: true, interlaced: true }))
+      .pipe(gulp.dest('dist/img'));
+  });
 
 gulp.task('usemin', function() {
-    return gulp.src('./*.html')
-    .pipe(flatmap(function(stream, file) {
-        return stream
+  return gulp.src('./*.html')
+  .pipe(flatmap(function(stream, file){
+      return stream
         .pipe(usemin({
-            css: [rev()],
-            html: [ function() {return htmlmin({ collapseWhitespace: true })}],
+            css: [ rev() ],
+            html: [ function() { return htmlmin({ collapseWhitespace: true })} ],
             js: [ uglify(), rev() ],
             inlinejs: [ uglify() ],
-            inlinecss: [ cleanCss(), 'concat']
+            inlinecss: [ cleanCss(), 'concat' ]
         }))
     }))
     .pipe(gulp.dest('dist/'));
-})
+});
 
-gulp.task('build', ['clean'], function() {
-    gulp.start('copyfonts', 'imagemin', 'usemin');
+gulp.task('build',['clean'], function() {
+    gulp.start('copyfonts','imagemin','usemin');
 });
